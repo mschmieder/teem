b6d2778765285afa857788d55ab0871b57163004
Matthias Schmieder <schmieder.matthias@gmail.com>
2017-05-07 19:10:05 +0200

    integrated build-tools added travis ci support

---
+++ travis/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PROJECT_NAME=$1
+OS=$2
+shift
+shift
+ARGS=$@
+
+set -e
+
+case $OS in
+docker-*)
+    CPP_BASE_BUILDSYSTEM_TAG=$(echo $OS | sed 's/docker-//g') 
+    ${SCRIPT_DIR}/../build-tools/cmakew_docker --container-name ${PROJECT_NAME} --docker-image-tag ${CPP_BASE_BUILDSYSTEM_TAG} ${ARGS}
+    exit $?
+    ;;
+macos | native )
+    bash ${SCRIPT_DIR}/../build-tools/cmakew ${ARGS}
+    exit $?
+    ;;
+*)
+    echo "Unsupported OS: $OS"
+    exit 1
+esac
