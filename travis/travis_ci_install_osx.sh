986401c6843856c352c123a56c6e75120c074aa3
Matthias Schmieder <schmieder.matthias@gmail.com>
2017-05-09 17:00:55 +0200

    adding clang-4.0 to path if running on macos

--- travis/travis_ci_install_osx.sh
+++ travis/travis_ci_install_osx.sh
@@ -38,15 +38,25 @@ fi
 which cmake &>/dev/null || install_brew_package cmake
 
 case "${COMPILER}" in
-gcc-4.8)       install_brew_package gcc@4.8 ;;
-gcc-4.9)       install_brew_package gcc@4.9 ;;
-gcc-5)         install_brew_package gcc@5 ;;
-gcc-6)         install_brew_package gcc@6 ;;
+gcc-4.8)
+    install_brew_package gcc@4.8 ;;
+gcc-4.9)
+    install_brew_package gcc@4.9 ;;
+gcc-5)
+    install_brew_package gcc@5 ;;
+gcc-6)         
+    install_brew_package gcc@6 ;;
 clang-default) ;;
-clang-3.7)     install_brew_package llvm@3.7 --with-clang --with-libcxx;;
-clang-3.8)     install_brew_package llvm@3.8 --with-clang --with-libcxx;;
-clang-3.9)     install_brew_package llvm@3.9 --with-clang --with-libcxx;;
-clang-4.0)     install_brew_package llvm     --with-clang --with-libcxx;;
+clang-3.7)     
+    install_brew_package llvm@3.7 --with-clang --with-libcxx;;
+clang-3.8)     
+    install_brew_package llvm@3.8 --with-clang --with-libcxx;;
+clang-3.9)     
+    install_brew_package llvm@3.9 --with-clang --with-libcxx;;
+clang-4.0)     
+    install_brew_package llvm     --with-clang --with-libcxx;;
+    echo 'export PATH="/usr/local/opt/llvm/bin/:$PATH"' >> ~/.bash_profile
+    
 *) echo "Compiler not supported: ${COMPILER}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
 
