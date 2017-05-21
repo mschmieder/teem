0a75b755e0e0937eedc23dd41b5854fa03e7c6d7
Matthias Schmieder <schmieder.matthias@gmail.com>
2017-05-21 12:05:28 +0200

    fixed paths to bin dirs

--- travis/travis_ci_install_osx.sh
+++ travis/travis_ci_install_osx.sh
@@ -45,36 +45,36 @@ which cmake &>/dev/null || install_brew_package cmake
 case "${COMPILER}" in
 gcc-4.8)
     install_brew_package gcc@4.8 
-    echo $(brew --prefix llvm@3.9) >> travis_env_paths
+    echo "$(brew --prefix llvm@3.9)/bin" >> travis_env_paths
     ;;
 gcc-4.9)
     install_brew_package gcc@4.9
-    echo $(brew --prefix gcc@4.9) >> travis_env_paths
+    echo "$(brew --prefix gcc@4.9)/bin" >> travis_env_paths
     ;;
 gcc-5)
     install_brew_package gcc@5 
-    echo $(brew --prefix gcc@5) >> travis_env_paths
+    echo "$(brew --prefix gcc@5)/bin" >> travis_env_paths
     ;;
 gcc-6)         
     install_brew_package gcc@6 
-    echo $(brew --prefix gcc@6) >> travis_env_paths
+    echo "$(brew --prefix gcc@6)/bin" >> travis_env_paths
     ;;
 clang-default) ;;
 clang-3.7)     
     install_brew_package llvm@3.7 --with-clang --with-libcxx
-    echo $(brew --prefix llvm@3.7) >> travis_env_paths
+    echo "$(brew --prefix llvm@3.7)/bin" >> travis_env_paths
     ;;
 clang-3.8)     
     install_brew_package llvm@3.8 --with-clang --with-libcxx
-    echo $(brew --prefix llvm@3.78) >> travis_env_paths
+    echo "$(brew --prefix llvm@3.8)/bin" >> travis_env_paths
     ;;
 clang-3.9)     
     install_brew_package llvm@3.9 --with-clang --with-libcxx
-    echo $(brew --prefix llvm@3.9) >> travis_env_paths
+    echo "$(brew --prefix llvm@3.9)/bin" >> travis_env_paths
     ;;
 clang-4.0)     
     install_brew_package llvm     --with-clang --with-libcxx
-    echo $(brew --prefix llvm) >> travis_env_paths
+    echo "$(brew --prefix llvm)/bin" >> travis_env_paths
     ;;
 *) echo "Compiler not supported: ${COMPILER}. See travis_ci_install_osx.sh"; exit 1 ;;
 esac
